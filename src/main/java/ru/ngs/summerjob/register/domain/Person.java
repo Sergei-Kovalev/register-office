7d587b62d1ca89f24e777ca553fe60ec64453169
Sergei Kovalev <summerjob@ngs.ru>
2022-11-07 19:01:32 +0300

    domain model added... watch domainModel.png

---
+++ src/main/java/ru/ngs/summerjob/register/domain/Person.java
@@ -0,0 +1,61 @@
+package ru.ngs.summerjob.register.domain;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Person {
+    private Long personId;
+    private String firstName;
+    private String LastName;
+    private String Patronymic;
+    private LocalDate dateOfBirth;
+    private List<Passport> passports;
+
+    public Long getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(Long personId) {
+        this.personId = personId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public String getPatronymic() {
+        return Patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        Patronymic = patronymic;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public List<Passport> getPassports() {
+        return passports;
+    }
+
+    public void setPassports(List<Passport> passports) {
+        this.passports = passports;
+    }
+}
