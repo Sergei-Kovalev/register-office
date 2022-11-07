4920e7f2bc7bd5ef310fffc269fcfe8c43b98600
Sergei Kovalev <summerjob@ngs.ru>
2022-11-07 17:16:58 +0300

    PersonManager added

--- src/main/java/ru/ngs/summerjob/register/manager/PersonManager.java
+++ src/main/java/ru/ngs/summerjob/register/manager/PersonManager.java
@@ -1,5 +1,8 @@
 package ru.ngs.summerjob.register.manager;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.Metadata;
@@ -8,10 +11,38 @@ import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import ru.ngs.summerjob.register.domain.Person;
 
+import java.util.List;
+
 public class PersonManager {
 
     public static void main(String[] args) {
 
+//        sessionExample();
+        
+        jpaExample();
+    }
+
+    private static void jpaExample() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("persistence");
+
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+
+        Person p = new Person();
+        p.setFirstName("Alexey");
+        p.setLastName("Fedorov");
+        em.persist(p);
+        System.out.println(p.getPersonId());
+        em.getTransaction().commit();
+        em.close();
+
+        em = emf.createEntityManager();
+        List<Person> from_person = em.createQuery("FROM Person", Person.class).getResultList();
+        from_person.forEach(System.out::println);
+        em.close();
+    }
+
+    private static void sessionExample() {
         SessionFactory sf = buildSessionFactory();
 
         Session session = sf.openSession();
@@ -22,6 +53,7 @@ public class PersonManager {
         p.setLastName("Sidorov");
 //        Long id = (Long) session.save(p);
         session.persist(p);                             //save - deprecated. persist works from hibernate 6.0
+        System.out.println(p.getPersonId());
 
         session.getTransaction().commit();
         session.close();
@@ -33,7 +65,8 @@ public class PersonManager {
         session.close();
 
         session = sf.openSession();
-        session.createQuery("FROM Person"); ///watch this tomorrow
+        List<Person> from_person = session.createQuery("FROM Person", Person.class).list();
+        from_person.forEach(System.out::println);
         session.close();
     }
 
